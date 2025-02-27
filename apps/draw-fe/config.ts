4eb75410fc4a12889ba2f1d1eee9c48b5bc31825
rayanjainn <rayansjain@gmail.com>
2025-02-27 16:11:53 +0530

    https

--- apps/draw-fe/config.ts
+++ apps/draw-fe/config.ts
@@ -1,6 +1,6 @@
-export const BACKEND_URL_PROD = "http://wdraw-http.iamray.xyz";
-export const WS_URL_PROD = "http://wdraw-ws.iamray.xyz";
-export const FE_URL_PROD = "http://wdraw.iamray.xyz";
+export const BACKEND_URL_PROD = "https://wdraw-http.iamray.xyz";
+export const WS_URL_PROD = "https://wdraw-ws.iamray.xyz";
+export const FE_URL_PROD = "https://wdraw.iamray.xyz";
 
 export const BACKEND_URL_DEV = "http://localhost:3001";
 export const WS_URL_DEV = "ws://localhost:8080";
